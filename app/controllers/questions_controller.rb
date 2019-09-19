2d9de0a65c5757c8bafe42c39cc3e47a8eb5e761
Tanaytatva <tanay.sharma@tatvasoft.com>
2019-09-19 20:18:37 +0530

    remove unwanted code

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,5 @@
 class QuestionsController < ApplicationController
 
-  def update
-  end
-
   def index
     questions = Question.all
     render json: questions.to_json( :include => [:mapping, :role] ), status: 200
