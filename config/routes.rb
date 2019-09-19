5a3f912f46b35c932ffba0b05afbb3d8397f3e9d
Tanaytatva <tanay.sharma@tatvasoft.com>
2019-09-19 19:23:25 +0530

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :questions
+  resources :questions, except: [:show, :new] do
+    collection do
+      get :mapping_index
+      get :role_index
+  end
 end
